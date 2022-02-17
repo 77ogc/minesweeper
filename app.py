58ad1da6d556e87681f5037164c627422b5eff01
Kevin Chang <04450@SERCOMM.COM>
2022-02-17 15:29:33 +0800

    All functional. TODO : Refactor

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+import tkinter.messagebox 
 
 import level_info  # level info
+
+DEBUG = 0
 		
 class App:
 	def __init__(self, master):
 		self.frame = tk.Frame(master)
+		self.frame.config(bg = '#efefef')
 		self.frame.pack()
 		self.menu()
 		self.virtual = tk.PhotoImage(file = "virtual.png")
@@ -13,35 +17,38 @@ class App:
 
 	def menu(self):
 		self.clean_view()
-		self.frame.config(width = 120, height = 150)
+		self.frame.config(width = 250, height = 150)
 		self.eazy_btn = tk.Button(self.frame, text = "EAZY", width = 10, command = self.eazy)
 		self.noraml_btn = tk.Button(self.frame, text = "NORMAL", width = 10, command = self.normal)
 		self.hard_btn = tk.Button(self.frame, text = "HARD", width = 10, command = self.hard)
-		self.eazy_btn.grid(column = 0, row = 0, padx = 20, pady = 10)
-		self.noraml_btn.grid(column = 0, row = 1, pady = 10)
-		self.hard_btn.grid(column = 0, row = 2, pady = 10)
+		self.eazy_btn.place(x = 90, y = 20)
+		self.noraml_btn.place(x = 90, y = 60)
+		self.hard_btn.place(x = 90, y = 100)	
+
 
 	def eazy(self):
 		level = self.game_info.EAZY
-		self.frame.config(width = 300, height = 300)
+		self.frame.config(width = 360, height = 370)
 		self.clean_view()
 		self.game_view(level)
 
 	def normal(self):
 		level = self.game_info.NORMAL
-		self.frame.config(width = 465, height = 475)
+		self.frame.config(width = 570, height = 585)
 		self.clean_view()
 		self.game_view(level)
 
 	def hard(self):
 		level = self.game_info.HARD
-		self.frame.config(width = 800, height = 500)
+		self.frame.config(width = 1000, height = 600)
 		self.clean_view()
 		self.game_view(level)
 
 	def game_view(self, level):
 		self.game_info_view(level)
 		self.game_grid_view(level)
+		if DEBUG:
+			self.debug()
 
 	def clean_view(self):
 		for widget in self.frame.winfo_children():
@@ -82,43 +89,83 @@ class App:
 		self.height = game_list[level][3]
 
 		self.grid_frame = tk.Frame(self.frame)
-		self.grid_frame.place(x = 50, y = 70)
+		self.grid_frame.place(x = 45, y = 70)
 		self.btn_list = [[0 for x in range(self.height)] for y in range(self.width)]
 		self.btn_val = [[0 for x in range(self.height)] for y in range(self.width)]
 		self.btn_is_flaged = [[0 for x in range(self.height)] for y in range(self.width)]
+		self.btn_is_visited = [[0 for x in range(self.height)] for y in range(self.width)]
 		
 		self.game_info.new_table(level)
 
-		display_table = self.game_info.get_diplay_table()
-		bomb_table =  self.game_info.get_bomb_table()
+		self.display_table = self.game_info.get_diplay_table()
+		self.bomb_table =  self.game_info.get_bomb_table()
+
+		self.win_condition = self.width * self.height
 		
 		for x in range(self.width):	
 			for y in range(self.height):
-				self.btn_val[x][y] = tk.IntVar(value = display_table[x][y])
-				self.btn_list[x][y] = tk.Button(self.grid_frame, image = self.virtual, width = 15, height = 15)
+				self.btn_list[x][y] = tk.Button(self.grid_frame, image = self.virtual, width = 20, height = 20, compound = 'c')
 				self.btn_list[x][y].bind("<Button-1>", lambda evt, x = x, y = y: self.left_click_for_flag(x, y))
 				self.btn_list[x][y].bind("<Button-3>", lambda evt, x = x, y = y: self.right_click_for_reveal(x, y))
 				self.btn_list[x][y].grid(column= x, row= y, padx = 1, pady = 1)
-				#self.btn_list[x][y].config(text = str(self.btn_val[x][y].get()), compound = 'c')
+				self.btn_list[x][y].config(text = " ")
 				
+		
+	
+	def win(self):
+		tk.messagebox.showerror(title = 'WIN', message = 'zzz')
+		self.menu()		
+
+	def show_dead_message(self):
+		tk.messagebox.showerror(title = 'DEAD', message = 'LOSER')
 
 
-
-	def test_bomb(self):
+	def debug(self):
 		for x in range(self.height):	
 			for y in range(self.width):
-				status = self.btn_val[x][y].get()
+				status = self.bomb_table[x][y]
+				self.btn_list[x][y].config(text = str(self.display_table[x][y]))
 				if status :
-					self.btn_list[x][y].config(bg = 'black')
+					self.btn_list[x][y].config(bg = 'yellow')
 
 
-	def left_click_for_flag(self, x, y):
-		#self.test_bomb()
-		if self.btn_is_flaged[x][y] : return
+	def recursive_update_display(self, x, y):
+		if x < 0 or y < 0 or x == self.width or y == self.height or self.btn_is_visited[x][y] or self.bomb_table[x][y]: return
+		self.btn_is_visited[x][y] = 1
+		self.win_condition -= 1;
+		status = self.display_table[x][y]
+
+		if status == 0:
+			self.btn_list[x][y].config(background = '#99ffcc',state = tk.DISABLED)
+			self.recursive_update_display(x+1,y)
+			self.recursive_update_display(x-1,y)
+			self.recursive_update_display(x,y+1)
+			self.recursive_update_display(x,y-1)
+		else:
+			self.btn_list[x][y].config(text = str(status), foreground = 'red', background = '#99ffcc')
+			
 
 		
 
-	
+	def left_click_for_flag(self, x, y):
+		#self.test_bomb()
+
+		# 1. check is flaged or not
+		if self.btn_is_flaged[x][y] : return
+
+		# 2. check if is a bomb
+		if self.bomb_table[x][y] :
+			print("BOOM")
+			self.show_dead_message()
+			self.menu()
+		# 3. check number
+		self.recursive_update_display(x,y)
+
+		# win condition
+		if self.win_condition == 0:
+			self.win()
+		
+
 	def right_click_for_reveal(self, x, y):
 
 		isFlaged = self.btn_is_flaged[x][y]
@@ -128,18 +175,24 @@ class App:
 			self.btn_list[x][y].config(image = self.virtual)
 			self.btn_is_flaged[x][y] = 0
 			self.fc.set(flag_count + 1)
+			self.win_condition += 1;
 			return
 
 		if flag_count != 0 :
 			self.btn_list[x][y].config(image = self.flag)
 			self.btn_is_flaged[x][y] = 1
 			self.fc.set(flag_count - 1)
-			return
+			self.win_condition -= 1;
+		
+		if self.win_condition == 0:
+			self.win()
 
 
 
 
 if __name__ == "__main__":
 	root = tk.Tk()
+	root.title("踩地雷")
+	root.resizable(0, 0)
 	app = App(root)
 	root.mainloop()
--- level_info.py
+++ level_info.py
@@ -37,15 +37,15 @@ class LevleInfo():
 		for x in range(col):	
 			for y in range(row):
 				if self.__bomb_table[x][y] == 1:
-					if x - 1 > 0 and y - 1 > 0 : self.__display_table[x-1][y-1] += 1
-					if y - 1 > 0 : self.__display_table[x][y-1] += 1
-					if x + 1 < col and y - 1 > 0 : self.__display_table[x+1][y-1] += 1
-					if x - 1 > 0 : self.__display_table[x-1][y] += 1
+					if x - 1 >= 0 and y - 1 >= 0 : self.__display_table[x-1][y-1] += 1
+					if y - 1 >= 0 : self.__display_table[x][y-1] += 1
+					if x + 1 < col and y - 1 >= 0 : self.__display_table[x+1][y-1] += 1
+					if x - 1 >= 0 : self.__display_table[x-1][y] += 1
 					if x + 1 < col : self.__display_table[x+1][y] += 1
-					if x - 1 > 0 and y + 1 < row : self.__display_table[x-1][y+1] += 1
+					if x - 1 >= 0 and y + 1 < row : self.__display_table[x-1][y+1] += 1
 					if y + 1 < row : self.__display_table[x][y+1] += 1
 					if x + 1 < col and y + 1 < row : self.__display_table[x+1][y+1] += 1
-		return self.__display_table
+		
 
 
 	def __cal_bomb_table(self,level):
@@ -53,16 +53,16 @@ class LevleInfo():
 			self.__bomb_table = [[0 for x in range(9)] for y in range(9)]
 			num = random.sample(range(81), 10)	
 			for i in num:
-				x = i / 9
-				y = i % 9
-				self.__bomb_table[int(x)][y] = 1
+				x = i % 9
+				y = i / 9
+				self.__bomb_table[x][int(y)] = 1
 		elif level == self.NORMAL:
 			self.__bomb_table = [[0 for x in range(16)] for y in range(16)]
 			num = random.sample(range(256), 40)	
 			for i in num:
-				x = i / 16
-				y = i % 16
-				self.__bomb_table[int(x)][y] = 1
+				x = i % 16
+				y = i / 16
+				self.__bomb_table[x][int(y)] = 1
 		elif level == self.HARD:
 			self.__bomb_table = [[0 for x in range(16)] for y in range(30)]
 			num = random.sample(range(480), 99)	
@@ -72,6 +72,6 @@ class LevleInfo():
 				self.__bomb_table[x][int(y)] = 1
 		
 
-		return self.__cal_bomb_arround_for_dispaly_table()
+		
 
 	
